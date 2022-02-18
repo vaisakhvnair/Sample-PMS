05356b72240da6faf0ab14dc3e064789a034d776
vaisakhvnair <vaisakhvnair1998@gmail.com>
2022-02-18 11:28:37 +0530

    test2

---
+++ sample/src/sample/test2.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class test2 {
+
+	public test2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test 2");
+
+	}
+
+}
